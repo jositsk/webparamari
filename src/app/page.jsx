96004cf67469afc6ce393b0765deb465d8042986
Josias Samael Olguin Rojas <130925399+jositsk@users.noreply.github.com>
2024-02-06 19:46:07 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -40,10 +40,14 @@ function Home() {
             sim
           </button>
           <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-6 rounded"
+            className=""
             onClick={handleNoClick}
           >
             {getNoButtonText()}
+          </button>
+
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-6 rounded">
+            <a href="https://download2445.mediafire.com/mfpecf00huqgbBg0deINe1DWH0kTVgaDODmiDilxUUjDk5H_tjzF6XBotLak2n0IiTQBmBp7Gv4Qr700f7JcAeg4tWPAQTfyH93LltTuI4MOludtJL2H83E2JKZf_pyHjKgzwL9zj7n4TX2basrjU1grcPFzPQ8h4UQg_yDq6JHvFGA/bhb6gxjxjjf86t5/MINECRAFT+PE+1.20.60+ATUALIZADO+ByHT.apk">Maincra</a>
           </button>
         </>
       )}
